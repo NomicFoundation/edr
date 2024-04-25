cdb7cc255ba43c830f937249bdfd48fb8f2e606c
Franco Victorio <victorio.franco@gmail.com>
2024-04-25 15:15:39 +0200

    Check that the workspace EDR version is used in hardhat-tests

---
+++ hardhat-tests/test/edr-override.ts
@@ -0,0 +1,24 @@
+import { assert } from "chai";
+import path from "path";
+
+// The tests under `hardhat-tests` assume that the EDR version used by
+// Hardhat is the one in the workspace, instead of the one installed
+// from npm. This test checks that this is the case.
+it("uses the workspace version of EDR", async function () {
+  const hardhatPath = require.resolve("hardhat");
+
+  const edrPath = require.resolve("@nomicfoundation/edr", {
+    paths: [hardhatPath],
+  });
+
+  const expectedPath = path.resolve(
+    __dirname,
+    "..",
+    "..",
+    "crates",
+    "edr_napi",
+    "index.js"
+  );
+
+  assert.equal(edrPath, expectedPath);
+});
