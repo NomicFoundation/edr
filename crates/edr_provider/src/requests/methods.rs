9fcd7df7b0ddadab572384a6bbd055fd90a4d0f3
Wodann <Wodann@users.noreply.github.com>
2024-05-07 14:56:21 +0000

    build: bump Rust to v1.78

--- crates/edr_provider/src/requests/methods.rs
+++ crates/edr_provider/src/requests/methods.rs
@@ -32,13 +32,13 @@ mod optional_block_spec {
 #[derive(Clone, Debug, PartialEq, serde::Deserialize, serde::Serialize)]
 #[serde(tag = "method", content = "params")]
 pub enum MethodInvocation {
-    /// eth_accounts
+    /// `eth_accounts`
     #[serde(rename = "eth_accounts", with = "edr_eth::serde::empty_params")]
     Accounts(()),
-    /// eth_blockNumber
+    /// `eth_blockNumber`
     #[serde(rename = "eth_blockNumber", with = "edr_eth::serde::empty_params")]
     BlockNumber(()),
-    /// eth_call
+    /// `eth_call`
     #[serde(rename = "eth_call")]
     Call(
         CallRequest,
@@ -49,13 +49,13 @@ pub enum MethodInvocation {
         Option<BlockSpec>,
         #[serde(default, skip_serializing_if = "Option::is_none")] Option<StateOverrideOptions>,
     ),
-    /// eth_chainId
+    /// `eth_chainId`
     #[serde(rename = "eth_chainId", with = "edr_eth::serde::empty_params")]
     ChainId(()),
-    /// eth_coinbase
+    /// `eth_coinbase`
     #[serde(rename = "eth_coinbase", with = "edr_eth::serde::empty_params")]
     Coinbase(()),
-    /// eth_estimateGas
+    /// `eth_estimateGas`
     #[serde(rename = "eth_estimateGas")]
     EstimateGas(
         CallRequest,
@@ -65,7 +65,7 @@ pub enum MethodInvocation {
         )]
         Option<BlockSpec>,
     ),
-    /// eth_feeHistory
+    /// `eth_feeHistory`
     #[serde(rename = "eth_feeHistory")]
     FeeHistory(
         /// block count
@@ -76,10 +76,10 @@ pub enum MethodInvocation {
         #[serde(default, skip_serializing_if = "Option::is_none")]
         Option<Vec<f64>>,
     ),
-    /// eth_gasPrice
+    /// `eth_gasPrice`
     #[serde(rename = "eth_gasPrice", with = "edr_eth::serde::empty_params")]
     GasPrice(()),
-    /// eth_getBalance
+    /// `eth_getBalance`
     #[serde(rename = "eth_getBalance")]
     GetBalance(
         #[serde(deserialize_with = "crate::requests::serde::deserialize_address")] Address,
@@ -89,14 +89,14 @@ pub enum MethodInvocation {
         )]
         Option<BlockSpec>,
     ),
-    /// eth_getBlockByNumber
+    /// `eth_getBlockByNumber`
     #[serde(rename = "eth_getBlockByNumber")]
     GetBlockByNumber(
         PreEip1898BlockSpec,
         /// include transaction data
         bool,
     ),
-    /// eth_getBlockByHash
+    /// `eth_getBlockByHash`
     #[serde(rename = "eth_getBlockByHash")]
     GetBlockByHash(
         /// hash
@@ -104,19 +104,19 @@ pub enum MethodInvocation {
         /// include transaction data
         bool,
     ),
-    /// eth_getBlockTransactionCountByHash
+    /// `eth_getBlockTransactionCountByHash`
     #[serde(
         rename = "eth_getBlockTransactionCountByHash",
         with = "edr_eth::serde::sequence"
     )]
     GetBlockTransactionCountByHash(B256),
-    /// eth_getBlockTransactionCountByNumber
+    /// `eth_getBlockTransactionCountByNumber`
     #[serde(
         rename = "eth_getBlockTransactionCountByNumber",
         with = "edr_eth::serde::sequence"
     )]
     GetBlockTransactionCountByNumber(PreEip1898BlockSpec),
-    /// eth_getCode
+    /// `eth_getCode`
     #[serde(rename = "eth_getCode")]
     GetCode(
         #[serde(deserialize_with = "crate::requests::serde::deserialize_address")] Address,
@@ -126,16 +126,16 @@ pub enum MethodInvocation {
         )]
         Option<BlockSpec>,
     ),
-    /// eth_getFilterChanges
+    /// `eth_getFilterChanges`
     #[serde(rename = "eth_getFilterChanges", with = "edr_eth::serde::sequence")]
     GetFilterChanges(U256),
-    /// eth_getFilterLogs
+    /// `eth_getFilterLogs`
     #[serde(rename = "eth_getFilterLogs", with = "edr_eth::serde::sequence")]
     GetFilterLogs(U256),
-    /// eth_getLogs
+    /// `eth_getLogs`
     #[serde(rename = "eth_getLogs", with = "edr_eth::serde::sequence")]
     GetLogs(LogFilterOptions),
-    /// eth_getStorageAt
+    /// `eth_getStorageAt`
     #[serde(rename = "eth_getStorageAt")]
     GetStorageAt(
         #[serde(deserialize_with = "crate::requests::serde::deserialize_address")] Address,
@@ -146,18 +146,18 @@ pub enum MethodInvocation {
         )]
         Option<BlockSpec>,
     ),
-    /// eth_getTransactionByBlockHashAndIndex
+    /// `eth_getTransactionByBlockHashAndIndex`
     #[serde(rename = "eth_getTransactionByBlockHashAndIndex")]
     GetTransactionByBlockHashAndIndex(B256, U256),
-    /// eth_getTransactionByBlockNumberAndIndex
+    /// `eth_getTransactionByBlockNumberAndIndex`
     // Matching Hardhat behavior in not accepting EIP-1898 block tags
     // https://github.com/NomicFoundation/hardhat/blob/06474681f72e1cd895abbec419f6f10be3d8e4ed/packages/hardhat-core/src/internal/hardhat-network/provider/modules/eth.ts#L775
     #[serde(rename = "eth_getTransactionByBlockNumberAndIndex")]
     GetTransactionByBlockNumberAndIndex(PreEip1898BlockSpec, U256),
-    /// eth_getTransactionByHash
+    /// `eth_getTransactionByHash`
     #[serde(rename = "eth_getTransactionByHash", with = "edr_eth::serde::sequence")]
     GetTransactionByHash(B256),
-    /// eth_getTransactionCount
+    /// `eth_getTransactionCount`
     #[serde(rename = "eth_getTransactionCount")]
     GetTransactionCount(
         #[serde(deserialize_with = "crate::requests::serde::deserialize_address")] Address,
@@ -167,125 +167,125 @@ pub enum MethodInvocation {
         )]
         Option<BlockSpec>,
     ),
-    /// eth_getTransactionReceipt
+    /// `eth_getTransactionReceipt`
     #[serde(
         rename = "eth_getTransactionReceipt",
         with = "edr_eth::serde::sequence"
     )]
     GetTransactionReceipt(B256),
-    /// eth_mining
+    /// `eth_mining`
     #[serde(rename = "eth_mining", with = "edr_eth::serde::empty_params")]
     Mining(()),
-    /// net_listening
+    /// `net_listening`
     #[serde(rename = "net_listening", with = "edr_eth::serde::empty_params")]
     NetListening(()),
-    /// net_peerCount
+    /// `net_peerCount`
     #[serde(rename = "net_peerCount", with = "edr_eth::serde::empty_params")]
     NetPeerCount(()),
-    /// net_version
+    /// `net_version`
     #[serde(rename = "net_version", with = "edr_eth::serde::empty_params")]
     NetVersion(()),
-    /// eth_newBlockFilter
+    /// `eth_newBlockFilter`
     #[serde(rename = "eth_newBlockFilter", with = "edr_eth::serde::empty_params")]
     NewBlockFilter(()),
-    /// eth_newFilter
+    /// `eth_newFilter`
     #[serde(rename = "eth_newFilter", with = "edr_eth::serde::sequence")]
     NewFilter(LogFilterOptions),
-    /// eth_newPendingTransactionFilter
+    /// `eth_newPendingTransactionFilter`
     #[serde(
         rename = "eth_newPendingTransactionFilter",
         with = "edr_eth::serde::empty_params"
     )]
     NewPendingTransactionFilter(()),
-    /// eth_pendingTransactions
+    /// `eth_pendingTransactions`
     #[serde(
         rename = "eth_pendingTransactions",
         with = "edr_eth::serde::empty_params"
     )]
     PendingTransactions(()),
-    /// eth_sendRawTransaction
+    /// `eth_sendRawTransaction`
     #[serde(rename = "eth_sendRawTransaction", with = "edr_eth::serde::sequence")]
     SendRawTransaction(Bytes),
-    /// eth_sendTransaction
+    /// `eth_sendTransaction`
     #[serde(rename = "eth_sendTransaction", with = "edr_eth::serde::sequence")]
     SendTransaction(EthTransactionRequest),
-    /// eth_sign
+    /// `eth_sign`
     #[serde(rename = "eth_sign", alias = "personal_sign")]
     Sign(
         Bytes,
         #[serde(deserialize_with = "crate::requests::serde::deserialize_address")] Address,
     ),
-    /// eth_signTypedData_v4
+    /// `eth_signTypedData_v4`
     #[serde(rename = "eth_signTypedData_v4")]
     SignTypedDataV4(
         #[serde(deserialize_with = "crate::requests::serde::deserialize_address")] Address,
         #[serde(deserialize_with = "crate::requests::serde::typed_data::deserialize")] TypedData,
     ),
-    /// eth_subscribe
+    /// `eth_subscribe`
     #[serde(rename = "eth_subscribe")]
     Subscribe(
         SubscriptionType,
         #[serde(default, skip_serializing_if = "Option::is_none")] Option<LogFilterOptions>,
     ),
-    /// eth_syncing
+    /// `eth_syncing`
     #[serde(rename = "eth_syncing", with = "edr_eth::serde::empty_params")]
     Syncing(()),
-    /// eth_uninstallFilter
+    /// `eth_uninstallFilter`
     #[serde(rename = "eth_uninstallFilter", with = "edr_eth::serde::sequence")]
     UninstallFilter(U256),
-    /// eth_unsubscribe
+    /// `eth_unsubscribe`
     #[serde(rename = "eth_unsubscribe", with = "edr_eth::serde::sequence")]
     Unsubscribe(U256),
-    /// web3_clientVersion
+    /// `web3_clientVersion`
     #[serde(rename = "web3_clientVersion", with = "edr_eth::serde::empty_params")]
     Web3ClientVersion(()),
-    /// web3_sha3
+    /// `web3_sha3`
     #[serde(rename = "web3_sha3", with = "edr_eth::serde::sequence")]
     Web3Sha3(Bytes),
-    /// evm_increaseTime
+    /// `evm_increaseTime`
     #[serde(rename = "evm_increaseTime", with = "edr_eth::serde::sequence")]
     EvmIncreaseTime(U64OrUsize),
-    /// evm_mine
+    /// `evm_mine`
     #[serde(
         rename = "evm_mine",
         serialize_with = "optional_single_to_sequence",
         deserialize_with = "sequence_to_optional_single"
     )]
     EvmMine(Option<U64OrUsize>),
-    /// evm_revert
+    /// `evm_revert`
     #[serde(rename = "evm_revert", with = "edr_eth::serde::sequence")]
     EvmRevert(U64),
-    /// evm_setAutomine
+    /// `evm_setAutomine`
     #[serde(rename = "evm_setAutomine", with = "edr_eth::serde::sequence")]
     EvmSetAutomine(bool),
-    /// evm_setBlockGasLimit
+    /// `evm_setBlockGasLimit`
     #[serde(rename = "evm_setBlockGasLimit", with = "edr_eth::serde::sequence")]
     EvmSetBlockGasLimit(U64),
-    /// evm_setIntervalMining
+    /// `evm_setIntervalMining`
     #[serde(rename = "evm_setIntervalMining", with = "edr_eth::serde::sequence")]
     EvmSetIntervalMining(IntervalConfig),
-    /// evm_setNextBlockTimestamp
+    /// `evm_setNextBlockTimestamp`
     #[serde(
         rename = "evm_setNextBlockTimestamp",
         with = "edr_eth::serde::sequence"
     )]
     EvmSetNextBlockTimestamp(U64OrUsize),
-    /// evm_snapshot
+    /// `evm_snapshot`
     #[serde(rename = "evm_snapshot", with = "edr_eth::serde::empty_params")]
     EvmSnapshot(()),
 
-    // debug_traceTransaction
+    // `debug_traceTransaction`
     #[serde(rename = "debug_traceCall")]
     DebugTraceCall(
         CallRequest,
         #[serde(default)] Option<BlockSpec>,
         #[serde(default)] Option<DebugTraceConfig>,
     ),
-    // debug_traceTransaction
+    // `debug_traceTransaction`
     #[serde(rename = "debug_traceTransaction")]
     DebugTraceTransaction(B256, #[serde(default)] Option<DebugTraceConfig>),
 
-    /// hardhat_addCompilationResult
+    /// `hardhat_addCompilationResult`
     #[serde(rename = "hardhat_addCompilationResult")]
     AddCompilationResult(
         /// solc version:
@@ -293,31 +293,31 @@ pub enum MethodInvocation {
         CompilerInput,
         CompilerOutput,
     ),
-    /// hardhat_dropTransaction
+    /// `hardhat_dropTransaction`
     #[serde(rename = "hardhat_dropTransaction", with = "edr_eth::serde::sequence")]
     DropTransaction(B256),
-    /// hardhat_getAutomine
+    /// `hardhat_getAutomine`
     #[serde(rename = "hardhat_getAutomine", with = "edr_eth::serde::empty_params")]
     GetAutomine(()),
-    /// hardhat_getStackTraceFailuresCount
+    /// `hardhat_getStackTraceFailuresCount`
     #[serde(
         rename = "hardhat_getStackTraceFailuresCount",
         with = "edr_eth::serde::empty_params"
     )]
     GetStackTraceFailuresCount(()),
-    /// hardhat_impersonateAccount
+    /// `hardhat_impersonateAccount`
     #[serde(
         rename = "hardhat_impersonateAccount",
         with = "edr_eth::serde::sequence"
     )]
     ImpersonateAccount(RpcAddress),
-    /// hardhat_intervalMine
+    /// `hardhat_intervalMine`
     #[serde(rename = "hardhat_intervalMine", with = "edr_eth::serde::empty_params")]
     IntervalMine(()),
-    /// hardhat_metadata
+    /// `hardhat_metadata`
     #[serde(rename = "hardhat_metadata", with = "edr_eth::serde::empty_params")]
     Metadata(()),
-    /// hardhat_mine
+    /// `hardhat_mine`
     #[serde(rename = "hardhat_mine")]
     Mine(
         /// block count:
@@ -331,44 +331,44 @@ pub enum MethodInvocation {
         )]
         Option<u64>,
     ),
-    /// hardhat_reset
+    /// `hardhat_reset`
     #[serde(
         rename = "hardhat_reset",
         serialize_with = "optional_single_to_sequence",
         deserialize_with = "sequence_to_optional_single"
     )]
     Reset(Option<ResetProviderConfig>),
-    /// hardhat_setBalance
+    /// `hardhat_setBalance`
     #[serde(rename = "hardhat_setBalance")]
     SetBalance(
         #[serde(deserialize_with = "crate::requests::serde::deserialize_address")] Address,
         #[serde(deserialize_with = "crate::requests::serde::deserialize_quantity")] U256,
     ),
-    /// hardhat_setCode
+    /// `hardhat_setCode`
     #[serde(rename = "hardhat_setCode")]
     SetCode(
         #[serde(deserialize_with = "crate::requests::serde::deserialize_address")] Address,
         #[serde(deserialize_with = "crate::requests::serde::deserialize_data")] Bytes,
     ),
-    /// hardhat_setCoinbase
+    /// `hardhat_setCoinbase`
     #[serde(rename = "hardhat_setCoinbase", with = "edr_eth::serde::sequence")]
     SetCoinbase(#[serde(deserialize_with = "crate::requests::serde::deserialize_address")] Address),
-    /// hardhat_setLoggingEnabled
+    /// `hardhat_setLoggingEnabled`
     #[serde(
         rename = "hardhat_setLoggingEnabled",
         with = "edr_eth::serde::sequence"
     )]
     SetLoggingEnabled(bool),
-    /// hardhat_setMinGasPrice
+    /// `hardhat_setMinGasPrice`
     #[serde(rename = "hardhat_setMinGasPrice", with = "edr_eth::serde::sequence")]
     SetMinGasPrice(U256),
-    /// hardhat_setNextBlockBaseFeePerGas
+    /// `hardhat_setNextBlockBaseFeePerGas`
     #[serde(
         rename = "hardhat_setNextBlockBaseFeePerGas",
         with = "edr_eth::serde::sequence"
     )]
     SetNextBlockBaseFeePerGas(U256),
-    /// hardhat_setNonce
+    /// `hardhat_setNonce`
     #[serde(rename = "hardhat_setNonce")]
     SetNonce(
         #[serde(deserialize_with = "crate::requests::serde::deserialize_address")] Address,
@@ -378,17 +378,17 @@ pub enum MethodInvocation {
         )]
         u64,
     ),
-    /// hardhat_setPrevRandao
+    /// `hardhat_setPrevRandao`
     #[serde(rename = "hardhat_setPrevRandao", with = "edr_eth::serde::sequence")]
     SetPrevRandao(B256),
-    /// hardhat_setStorageAt
+    /// `hardhat_setStorageAt`
     #[serde(rename = "hardhat_setStorageAt")]
     SetStorageAt(
         #[serde(deserialize_with = "crate::requests::serde::deserialize_address")] Address,
         #[serde(deserialize_with = "crate::requests::serde::deserialize_storage_key")] U256,
         #[serde(with = "crate::requests::serde::storage_value")] U256,
     ),
-    /// hardhat_stopImpersonatingAccount
+    /// `hardhat_stopImpersonatingAccount`
     #[serde(
         rename = "hardhat_stopImpersonatingAccount",
         with = "edr_eth::serde::sequence"
