1c7e0f467b7d5c845a7fe30b8ac36941438b0e2a
Igor Matuszewski <xanewok@gmail.com>
2024-06-07 10:55:45 +0200

    tests: Add TS integration tests

--- hardhat-tests/test/internal/hardhat-network/provider/modules/evm.ts
+++ hardhat-tests/test/internal/hardhat-network/provider/modules/evm.ts
@@ -146,6 +146,21 @@ describe("Evm module", function () {
           assert.strictEqual(totalOffset1, originalOffset + offset1);
           assert.strictEqual(totalOffset2, originalOffset + offset1 + offset2);
         });
+
+        it("should provide a reasonable error for big numbers", async function () {
+          await assertInvalidArgumentsError(
+            this.provider,
+            "evm_increaseTime",
+            ["10000000000000000000000000000000"],
+            "Timestamp must be a positive integer or a string not exceeding 2^64"
+          );
+          await assertInvalidArgumentsError(
+            this.provider,
+            "evm_increaseTime",
+            ["0x1111111111111111111111111111111111111"],
+            "Timestamp must be a positive integer or a string not exceeding 2^64"
+          );
+        });
       });
 
       describe("evm_setNextBlockTimestamp", function () {
@@ -317,6 +332,21 @@ describe("Evm module", function () {
               );
 
               assertQuantity(block.timestamp, timestamp);
+            });
+
+            it("should provide a reasonable error for big numbers", async function () {
+              await assertInvalidArgumentsError(
+                this.provider,
+                "evm_setNextBlockTimestamp",
+                ["10000000000000000000000000000000"],
+                "Timestamp must be a positive integer or a string not exceeding 2^64"
+              );
+              await assertInvalidArgumentsError(
+                this.provider,
+                "evm_setNextBlockTimestamp",
+                ["0x1111111111111111111111111111111111111"],
+                "Timestamp must be a positive integer or a string not exceeding 2^64"
+              );
             });
 
             describe("When the initial date is in the past", function () {
@@ -610,6 +640,21 @@ describe("Evm module", function () {
           );
 
           assertQuantity(block.timestamp, timestamp);
+        });
+
+        it("should provide a reasonable error for big numbers", async function () {
+          await assertInvalidArgumentsError(
+            this.provider,
+            "evm_mine",
+            ["10000000000000000000000000000000"],
+            "Timestamp must be a positive integer or a string not exceeding 2^64"
+          );
+          await assertInvalidArgumentsError(
+            this.provider,
+            "evm_mine",
+            ["0x1111111111111111111111111111111111111"],
+            "Timestamp must be a positive integer or a string not exceeding 2^64"
+          );
         });
 
         describe.skip("tests using sinon", () => {
