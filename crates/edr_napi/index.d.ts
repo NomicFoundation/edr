3882f358973a89eb4324567b689047dc58145414
Franco Victorio <victorio.franco@gmail.com>
2024-08-05 17:03:59 +0200

    fix: add json alias property in provider response

--- crates/edr_napi/index.d.ts
+++ crates/edr_napi/index.d.ts
@@ -420,6 +420,7 @@ export class Provider {
 export class Response {
   /** Returns the response data as a JSON string or a JSON object. */
   get data(): string | any
+  get json(): string | any
   get solidityTrace(): RawTrace | null
   get traces(): Array<RawTrace>
 }
