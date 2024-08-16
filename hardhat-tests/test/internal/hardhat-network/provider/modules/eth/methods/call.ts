9703bce9b46dbe0c4b9cfba7c75e910d620961d6
Wodann <Wodann@users.noreply.github.com>
2024-08-16 23:31:14 +0000

    fix: length of blobs

--- hardhat-tests/test/internal/hardhat-network/provider/modules/eth/methods/call.ts
+++ hardhat-tests/test/internal/hardhat-network/provider/modules/eth/methods/call.ts
@@ -1,3 +1,4 @@
+import { bytesToHex as bufferToHex } from "@nomicfoundation/ethereumjs-util";
 import { assert } from "chai";
 import { ethers } from "ethers";
 import {
@@ -1611,7 +1612,7 @@ contract C {
               {
                 from: DEFAULT_ACCOUNTS_ADDRESSES[1],
                 to: DEFAULT_ACCOUNTS_ADDRESSES[2],
-                blobs: ["0x1234"],
+                blobs: [bufferToHex(new Uint8Array(131072))],
                 blobVersionedHashes: [
                   "0x1234567812345678123456781234567812345678123456781234567812345678",
                 ],
