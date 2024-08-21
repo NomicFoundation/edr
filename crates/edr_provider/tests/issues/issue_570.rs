b11aac74a3a5a53db664a29f0799acd64abec75a
Agost Biro <agostbiro@gmail.com>
2024-08-21 12:40:55 +0200

    Fix test setup

--- crates/edr_provider/tests/issues/issue_570.rs
+++ crates/edr_provider/tests/issues/issue_570.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr as _;
+use std::{convert::Infallible, str::FromStr as _};
 
 use edr_eth::{spec::HardforkActivations, SpecId, B256};
 use edr_provider::{
@@ -6,22 +6,22 @@ use edr_provider::{
     MethodInvocation, NoopLogger, Provider, ProviderError, ProviderRequest,
 };
 use edr_test_utils::env::get_alchemy_url;
+use serial_test::serial;
 use tokio::runtime;
 
-// `eth_debugTraceTransaction` should return a helpful error message if there is
-// a transaction in the block whose type is not supported.
-// https://github.com/NomicFoundation/edr/issues/570
-#[tokio::test(flavor = "multi_thread")]
-async fn issue_570_error_message() -> anyhow::Result<()> {
+// SAFETY: tests that modify the environment should be run serially.
+
+fn get_provider() -> anyhow::Result<Provider<Infallible>> {
     // Base Sepolia Testnet
     const CHAIN_ID: u64 = 84532;
+    const BLOCK_NUMBER: u64 = 13_560_400;
 
     let logger = Box::new(NoopLogger);
     let subscriber = Box::new(|_event| {});
 
     let mut config = create_test_config_with_fork(Some(ForkConfig {
         json_rpc_url: get_alchemy_url().replace("eth-mainnet", "base-sepolia"),
-        block_number: Some(13_560_400),
+        block_number: Some(BLOCK_NUMBER),
         http_headers: None,
     }));
 
@@ -31,13 +31,22 @@ async fn issue_570_error_message() -> anyhow::Result<()> {
 
     config.chain_id = CHAIN_ID;
 
-    let provider = Provider::new(
+    Ok(Provider::new(
         runtime::Handle::current(),
         logger,
         subscriber,
         config,
         CurrentTime,
-    )?;
+    )?)
+}
+
+// `eth_debugTraceTransaction` should return a helpful error message if there is
+// a transaction in the block whose type is not supported.
+// https://github.com/NomicFoundation/edr/issues/570
+#[serial]
+#[tokio::test(flavor = "multi_thread")]
+async fn issue_570_error_message() -> anyhow::Result<()> {
+    let provider = get_provider()?;
 
     let transaction_hash =
         B256::from_str("0xe565eb3bfd815efcc82bed1eef580117f9dc3d6896db42500572c8e789c5edd4")?;
@@ -61,34 +70,11 @@ async fn issue_570_error_message() -> anyhow::Result<()> {
 // `eth_debugTraceTransaction` should ignore transactions with unsupported types
 // if a custom environment variable is set.
 // https://github.com/NomicFoundation/edr/issues/570
+#[serial]
 #[tokio::test(flavor = "multi_thread")]
 async fn issue_570_env_var() -> anyhow::Result<()> {
-    let logger = Box::new(NoopLogger);
-    let subscriber = Box::new(|_event| {});
-
-    let mut config = create_test_config_with_fork(Some(ForkConfig {
-        json_rpc_url: get_alchemy_url().replace("eth-mainnet", "base-sepolia"),
-        block_number: Some(13_560_400),
-        http_headers: None,
-    }));
-
-    let chain_id = 84532;
-
-    config
-        .chains
-        .insert(chain_id, HardforkActivations::with_spec_id(SpecId::CANCUN));
-
-    // The default chain id set by Hardhat
-    config.chain_id = chain_id;
-
     std::env::set_var("__EDR_UNSAFE_SKIP_UNSUPPORTED_TRANSACTION_TYPES", "true");
-    let provider = Provider::new(
-        runtime::Handle::current(),
-        logger,
-        subscriber,
-        config,
-        CurrentTime,
-    );
+    let provider = get_provider();
     std::env::remove_var("__EDR_UNSAFE_SKIP_UNSUPPORTED_TRANSACTION_TYPES");
     let provider = provider?;
 
@@ -106,34 +92,11 @@ async fn issue_570_env_var() -> anyhow::Result<()> {
 
 // `eth_debugTraceTransaction` should return a helpful error message if tracing
 // is requested for a transaction with an unsupported type. https://github.com/NomicFoundation/edr/issues/570
+#[serial]
 #[tokio::test(flavor = "multi_thread")]
 async fn issue_570_unsupported_requested() -> anyhow::Result<()> {
-    let logger = Box::new(NoopLogger);
-    let subscriber = Box::new(|_event| {});
-
-    let mut config = create_test_config_with_fork(Some(ForkConfig {
-        json_rpc_url: get_alchemy_url().replace("eth-mainnet", "base-sepolia"),
-        block_number: Some(13_560_400),
-        http_headers: None,
-    }));
-
-    let chain_id = 84532;
-
-    config
-        .chains
-        .insert(chain_id, HardforkActivations::with_spec_id(SpecId::CANCUN));
-
-    // The default chain id set by Hardhat
-    config.chain_id = chain_id;
-
     std::env::set_var("__EDR_UNSAFE_SKIP_UNSUPPORTED_TRANSACTION_TYPES", "true");
-    let provider = Provider::new(
-        runtime::Handle::current(),
-        logger,
-        subscriber,
-        config,
-        CurrentTime,
-    );
+    let provider = get_provider();
     std::env::remove_var("__EDR_UNSAFE_SKIP_UNSUPPORTED_TRANSACTION_TYPES");
     let provider = provider?;
 
