fc7176655aba4c83a4dd14719aa76402cb4926d0
Agost Biro <agostbiro@gmail.com>
2024-08-22 18:23:37 +0200

    Remove hard coded api keys

--- crates/foundry/evm/core/src/fork/database.rs
+++ crates/foundry/evm/core/src/fork/database.rs
@@ -296,7 +296,7 @@ mod tests {
     /// return the `AccountInfo`
     #[tokio::test(flavor = "multi_thread")]
     async fn fork_db_insert_basic_default() {
-        let rpc = edr_test_utils::rpc::next_http_rpc_endpoint();
+        let rpc = edr_test_utils::rpc::http_rpc_endpoint();
         let provider = get_http_provider(rpc.clone());
         let meta = BlockchainDbMeta {
             cfg_env: CfgEnv::default(),
--- crates/foundry/forge/tests/it/fork.rs
+++ crates/foundry/forge/tests/it/fork.rs
@@ -72,7 +72,7 @@ async fn test_rpc_fork() {
 /// Tests that we can launch in forking mode
 #[tokio::test(flavor = "multi_thread")]
 async fn test_launch_fork() {
-    let rpc_url = edr_test_utils::rpc::next_http_archive_rpc_endpoint();
+    let rpc_url = edr_test_utils::rpc::http_archive_rpc_endpoint();
     let runner = TEST_DATA_DEFAULT.forked_runner(&rpc_url).await;
     let filter = SolidityTestFilter::new(".*", ".*", &format!(".*fork{RE_PATH_SEPARATOR}Launch"));
     TestConfig::with_filter(runner, filter).run().await;
@@ -81,7 +81,7 @@ async fn test_launch_fork() {
 /// Smoke test that forking workings with websockets
 #[tokio::test(flavor = "multi_thread")]
 async fn test_launch_fork_ws() {
-    let rpc_url = edr_test_utils::rpc::next_ws_archive_rpc_endpoint();
+    let rpc_url = edr_test_utils::rpc::ws_archive_rpc_endpoint();
     let runner = TEST_DATA_DEFAULT.forked_runner(&rpc_url).await;
     let filter = SolidityTestFilter::new(".*", ".*", &format!(".*fork{RE_PATH_SEPARATOR}Launch"));
     TestConfig::with_filter(runner, filter).run().await;
