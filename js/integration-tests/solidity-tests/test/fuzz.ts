7fe83157fbf35a267350a94199c93b41a49085b8
Franco Victorio <victorio.franco@gmail.com>
2024-09-12 09:54:04 +0200

    Remove chai-as-promised
    We can use fs.existsSync to check if a directory exists

--- js/integration-tests/solidity-tests/test/fuzz.ts
+++ js/integration-tests/solidity-tests/test/fuzz.ts
@@ -1,10 +1,8 @@
 import chai, { assert, expect } from "chai";
-import chaiAsPromised from "chai-as-promised";
 import { TestContext } from "./testContext";
 import fs from "node:fs/promises";
+import { existsSync } from "node:fs";
 import { FuzzConfigArgs } from "@nomicfoundation/edr";
-
-chai.use(chaiAsPromised);
 
 describe("Fuzz and invariant testing", function () {
   let testContext: TestContext;
@@ -27,7 +25,7 @@ describe("Fuzz and invariant testing", function () {
     assert.equal(result1.totalTests, 1);
 
     // The fuzz failure directory should not be created if we don't set the directory
-    await assert.isRejected(fs.stat(failureDir));
+    assert.isFalse(existsSync(failureDir));
 
     const result2 = await testContext.runTestsWithStats("OverflowTest", {
       fuzz: {
@@ -38,7 +36,7 @@ describe("Fuzz and invariant testing", function () {
     assert.equal(result2.totalTests, 1);
 
     // The fuzz failure directory should now be created
-    await assert.isFulfilled(fs.stat(failureDir));
+    assert.isTrue(existsSync(failureDir));
   });
 
   // One test as steps should be sequential
@@ -66,7 +64,7 @@ describe("Fuzz and invariant testing", function () {
     assert.equal(result1.totalTests, 1);
 
     // The invariant failure directory should not be created if we don't set the directory
-    await assert.isRejected(fs.stat(failureDir));
+    assert.isFalse(existsSync(failureDir));
 
     const firstStart = performance.now();
     const result2 = await testContext.runTestsWithStats(
@@ -83,7 +81,7 @@ describe("Fuzz and invariant testing", function () {
     assert.equal(result2.totalTests, 1);
 
     // The invariant failure directory should now be created
-    await assert.isFulfilled(fs.stat(failureDir));
+    assert.isTrue(existsSync(failureDir));
 
     const secondStart = performance.now();
     const result3 = await testContext.runTestsWithStats(
@@ -100,7 +98,7 @@ describe("Fuzz and invariant testing", function () {
     const secondDuration = performance.now() - secondStart;
 
     // The invariant failure directory should still be there
-    await assert.isFulfilled(fs.stat(failureDir));
+    assert.isTrue(existsSync(failureDir));
     // The second run should be faster than the first because it should use the persisted failure.
     assert.isBelow(secondDuration, firstDuration * 0.25);
   });
