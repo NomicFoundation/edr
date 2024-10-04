bcb77bc89d45f09bf63475844b824f6a48f4485c
Wodann <Wodann@users.noreply.github.com>
2024-10-04 06:26:04 +0000

    refactor: use const constructors for beacon root address and bytecode

--- crates/edr_eth/src/beacon.rs
+++ crates/edr_eth/src/beacon.rs
@@ -1,5 +1,9 @@
+use revm_primitives::hex;
+
+use crate::{address, Address};
+
 /// The address of the beacon roots contract.
-pub const BEACON_ROOTS_ADDRESS: &str = "0x000F3df6D732807Ef1319fB7B8bB8522d0Beac02";
+pub const BEACON_ROOTS_ADDRESS: Address = address!("000F3df6D732807Ef1319fB7B8bB8522d0Beac02");
 
 /// The bytecode of the beacon roots contract.
-pub const BEACON_ROOTS_BYTECODE: &str = "0x3373fffffffffffffffffffffffffffffffffffffffe14604d57602036146024575f5ffd5b5f35801560495762001fff810690815414603c575f5ffd5b62001fff01545f5260205ff35b5f5ffd5b62001fff42064281555f359062001fff015500";
+pub const BEACON_ROOTS_BYTECODE: [u8; 97] = hex!("3373fffffffffffffffffffffffffffffffffffffffe14604d57602036146024575f5ffd5b5f35801560495762001fff810690815414603c575f5ffd5b62001fff01545f5260205ff35b5f5ffd5b62001fff42064281555f359062001fff015500");
