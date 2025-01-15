5bc409f17a255a4715ea184fd50cb065ebaaaf07
Agost Biro <agostbiro@gmail.com>
2025-01-15 13:08:40 +0000

    cleanup

--- js/integration-tests/solidity-tests/hardhat.config.ts
+++ js/integration-tests/solidity-tests/hardhat.config.ts
@@ -1,35 +1,6 @@
-import { task } from "hardhat/config";
-import fs from "node:fs";
-import path from "node:path";
-
-const RUST_INTEGRATION_TEST_DATA_PATH =
-  "../../../crates/edr_solidity_tests/tests/testdata";
-
-// HACK: copy the libraries Rust integration tests into the Hardhat project source
-task("copyLibraries", "Run pre-test script", async (taskArgs, hre) => {
-  await fs.promises.copyFile(
-    path.join(RUST_INTEGRATION_TEST_DATA_PATH, "lib/ds-test/src/test.sol"),
-    path.join(hre.config.paths.sources, "test.sol")
-  );
-  await fs.promises.copyFile(
-    path.join(RUST_INTEGRATION_TEST_DATA_PATH, "cheats/Vm.sol"),
-    path.join(hre.config.paths.sources, "Vm.sol")
-  );
-});
-
-task("test").setAction(async (taskArgs, hre, runSuper) => {
-  // Run the pretest task before tests
-  await hre.run("copyLibraries");
-  // Run the actual tests
-  await runSuper();
-});
-
 export default {
   solidity: {
     version: "0.8.24",
     settings: { evmVersion: "cancun" },
-  },
-  compilerOptions: {
-    paths: { "forge-std": ["node_modules/forge-std/src/"] },
   },
 };
