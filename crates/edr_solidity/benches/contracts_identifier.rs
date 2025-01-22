0a99d3bca7326421667acbb478c7bc0a3166764f
Agost Biro <agostbiro@gmail.com>
2025-01-22 15:19:40 +0000

    Fix CI

--- crates/edr_solidity/benches/contracts_identifier.rs
+++ crates/edr_solidity/benches/contracts_identifier.rs
@@ -1,13 +1,13 @@
 //! Benchmark for construcing a contract identifier.
 //!
 //! Steps to run:
-//! 1. Check out https://github.com/NomicFoundation/forge-std/tree/js-benchmark-config
+//! 1. Check out <https://github.com/NomicFoundation/forge-std/tree/js-benchmark-config>
 //!    locally (note the branch).
 //! 2. In the `forge-std` repo root:
 //! 2.1. `npm i`
 //! 2.2. `npx hardhat compile`
 //! 3. In the `crates/edr_solidity` directory:
-//! 3.1. `export FORGE_STD_ARTIFACTS_DIR=/path/to/forge-std/artifacts`
+//! 3.1. `export EDR_FORGE_STD_ARTIFACTS_DIR=/path/to/forge-std/artifacts`
 //! 3.2. `cargo bench contracts_identifier`
 use std::{fs, path::PathBuf, time::Duration};
 
@@ -17,8 +17,13 @@ use edr_solidity::{
     contract_decoder::{BuildInfoConfig, ContractDecoder},
 };
 
-fn load_build_info_config() -> anyhow::Result<BuildInfoConfig> {
-    let artifacts_dir = std::env::var("FORGE_STD_ARTIFACTS_DIR")?;
+const FORGE_STD_ARTIFACTS_DIR: &str = "EDR_FORGE_STD_ARTIFACTS_DIR";
+
+fn load_build_info_config() -> anyhow::Result<Option<BuildInfoConfig>> {
+    let Ok(artifacts_dir) = std::env::var(FORGE_STD_ARTIFACTS_DIR) else {
+        println!("Skipping contracts identifier benchmark as {FORGE_STD_ARTIFACTS_DIR} environment variable is not set");
+        return Ok(None);
+    };
     let build_info_dir = PathBuf::from(&artifacts_dir).join("build-info");
 
     let mut build_infos = Vec::new();
@@ -33,16 +38,17 @@ fn load_build_info_config() -> anyhow::Result<BuildInfoConfig> {
         }
     }
 
-    println!("build infos len: {}", build_infos.len());
-
-    Ok(BuildInfoConfig {
+    Ok(Some(BuildInfoConfig {
         build_infos: Some(build_infos),
         ignore_contracts: None,
-    })
+    }))
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let build_info_config = load_build_info_config().expect("loads build info config");
+    let Some(build_info_config) = load_build_info_config().expect("loads build info config") else {
+        return;
+    };
+
     let contracts = &build_info_config
         .build_infos
         .as_ref()
@@ -53,17 +59,15 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         .contracts;
 
     // Sanity check
-    let total_contracts = contracts.iter().map(|(k, v)| v.len()).sum::<usize>();
+    let total_contracts = contracts.iter().map(|(_k, v)| v.len()).sum::<usize>();
     let min_contracts = 70;
-    if total_contracts < min_contracts {
-        panic!(
-            "Expected at least {} contracts, instead it is {}",
-            min_contracts, total_contracts
-        );
-    }
+    assert!(
+        total_contracts >= min_contracts,
+        "Expected at least {min_contracts} contracts, instead it is {total_contracts}",
+    );
 
     c.bench_function("initialize_contracts_identifier", |b| {
-        b.iter(|| ContractDecoder::new(black_box(&build_info_config)).unwrap())
+        b.iter(|| ContractDecoder::new(black_box(&build_info_config)).unwrap());
     });
 }
 
