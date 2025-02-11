da4d620530a93787a7c6f1eccac7af3f7862e2f4
Franco Victorio <victorio.franco@gmail.com>
2025-02-11 18:51:56 +0100

    feat: add basic support for OP mainnet predeploys (#798)
    Co-authored-by: Wodann <Wodann@users.noreply.github.com>

--- crates/edr_napi/test/optimism.ts
+++ crates/edr_napi/test/optimism.ts
@@ -186,5 +186,119 @@ describe("Multi-chain", () => {
 
       await assert.isFulfilled(block);
     });
+
+    describe("Predeploys", () => {
+      it("should have the GasPriceOracle predeploy", async function () {
+        const provider = await context.createProvider(
+          OPTIMISM_CHAIN_TYPE,
+          {
+            genesisState: optimismGenesisState(
+              optimismHardforkFromString(providerConfig.hardfork)
+            ),
+            ...providerConfig,
+          },
+          loggerConfig,
+          {
+            subscriptionCallback: (_event: SubscriptionEvent) => {},
+          },
+          {}
+        );
+
+        const response = await provider.handleRequest(
+          JSON.stringify({
+            id: 1,
+            jsonrpc: "2.0",
+            method: "eth_call",
+            params: [
+              {
+                to: "0x420000000000000000000000000000000000000F",
+                data: "0x960e3a23", // isFjord()
+              },
+            ],
+          })
+        );
+        const responseData = JSON.parse(response.data);
+
+        assert.equal(
+          responseData.result,
+          "0x0000000000000000000000000000000000000000000000000000000000000001"
+        );
+      });
+
+      it("should have the L1Block predeploy", async function () {
+        const provider = await context.createProvider(
+          OPTIMISM_CHAIN_TYPE,
+          {
+            genesisState: optimismGenesisState(
+              optimismHardforkFromString(providerConfig.hardfork)
+            ),
+            ...providerConfig,
+          },
+          loggerConfig,
+          {
+            subscriptionCallback: (_event: SubscriptionEvent) => {},
+          },
+          {}
+        );
+
+        const response = await provider.handleRequest(
+          JSON.stringify({
+            id: 1,
+            jsonrpc: "2.0",
+            method: "eth_call",
+            params: [
+              {
+                to: "0x4200000000000000000000000000000000000015",
+                data: "0x5cf24969", // basefee()
+              },
+            ],
+          })
+        );
+        const responseData = JSON.parse(response.data);
+
+        assert.equal(
+          responseData.result,
+          "0x00000000000000000000000000000000000000000000000000000002540be400"
+        ); // 10 gwei
+      });
+
+      it("should stub unimplemented predeploys", async function () {
+        const provider = await context.createProvider(
+          OPTIMISM_CHAIN_TYPE,
+          {
+            genesisState: optimismGenesisState(
+              optimismHardforkFromString(providerConfig.hardfork)
+            ),
+            ...providerConfig,
+          },
+          loggerConfig,
+          {
+            subscriptionCallback: (_event: SubscriptionEvent) => {},
+          },
+          {}
+        );
+
+        const response = await provider.handleRequest(
+          JSON.stringify({
+            id: 1,
+            jsonrpc: "2.0",
+            method: "eth_call",
+            params: [
+              {
+                to: "0x4200000000000000000000000000000000000016", // L2ToL1MessagePasser
+                data: "0x3f827a5a", // MESSAGE_VERSION()
+              },
+            ],
+          })
+        );
+        const responseData = JSON.parse(response.data);
+
+        assert.equal(
+          responseData.result,
+          // Error("Predeploy L2ToL1MessagePasser is not supported.")
+          "0x08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002f5072656465706c6f79204c32546f4c314d657373616765506173736572206973206e6f7420737570706f727465642e0000000000000000000000000000000000"
+        );
+      });
+    });
   });
 });
