d7dc8b79b17313bd9d904465fb165f993188ee47
Francisco Giordano <fg@frang.io>
2025-04-29 20:17:55 -0300

    add base full block test

--- crates/edr_op/tests/integration/full_block.rs
+++ crates/edr_op/tests/integration/full_block.rs
@@ -3,31 +3,36 @@
 use edr_evm::impl_full_block_tests;
 use edr_op::OpChainSpec;
 
-use super::op::mainnet_url;
+use super::{op, base};
 
 impl_full_block_tests! {
-    mainnet_regolith => OpChainSpec {
+    op_mainnet_regolith => OpChainSpec {
         block_number: 105_235_064,
-        url: mainnet_url(),
+        url: op::mainnet_url(),
     },
-    mainnet_canyon => OpChainSpec {
+    op_mainnet_canyon => OpChainSpec {
         block_number: 115_235_064,
-        url: mainnet_url(),
+        url: op::mainnet_url(),
     },
-    mainnet_ecotone => OpChainSpec {
+    op_mainnet_ecotone => OpChainSpec {
         block_number: 121_874_088,
-        url: mainnet_url(),
+        url: op::mainnet_url(),
     },
-    mainnet_fjord => OpChainSpec {
+    op_mainnet_fjord => OpChainSpec {
         block_number: 122_514_212,
-        url: mainnet_url(),
+        url: op::mainnet_url(),
     },
-    mainnet_granite => OpChainSpec {
+    op_mainnet_granite => OpChainSpec {
         block_number: 125_235_823,
-        url: mainnet_url(),
+        url: op::mainnet_url(),
     },
-    mainnet_holocene => OpChainSpec {
+    op_mainnet_holocene => OpChainSpec {
         block_number: 130_423_412,
-        url: mainnet_url(),
+        url: op::mainnet_url(),
+    },
+
+    base_mainnet_holocene => OpChainSpec {
+        block_number: 24_828_127,
+        url: base::mainnet_url(),
     },
 }
