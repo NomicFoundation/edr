1e10d475aa73acaf7f167afc274b2055799206d1
Francisco Giordano <fg@frang.io>
2025-06-10 16:28:10 -0300

    fix clippy

--- crates/edr_provider/src/data.rs
+++ crates/edr_provider/src/data.rs
@@ -91,32 +91,31 @@ const DEFAULT_SKIP_UNSUPPORTED_TRANSACTION_TYPES: bool = false;
 
 // Common test accounts that have had their private keys leaked.
 static LEAKED_ADDRESSES: LazyLock<HashSet<Address>> = LazyLock::new(|| {
-    HashSet::from_iter(
-        [
-            hex!("f39fd6e51aad88f6f4ce6ab8827279cfffb92266"),
-            hex!("70997970c51812dc3a010c7d01b50e0d17dc79c8"),
-            hex!("3c44cdddb6a900fa2b585dd299e03d12fa4293bc"),
-            hex!("90f79bf6eb2c4f870365e785982e1f101e93b906"),
-            hex!("15d34aaf54267db7d7c367839aaf71a00a2c6a65"),
-            hex!("9965507d1a55bcc2695c58ba16fb37d819b0a4dc"),
-            hex!("976ea74026e726554db657fa54763abd0c3a0aa9"),
-            hex!("14dc79964da2c08b23698b3d3cc7ca32193d9955"),
-            hex!("23618e81e3f5cdf7f54c3d65f7fbc0abf5b21e8f"),
-            hex!("a0ee7a142d267c1f36714e4a8f75612f20a79720"),
-            hex!("bcd4042de499d14e55001ccbb24a551f3b954096"),
-            hex!("71be63f3384f5fb98995898a86b02fb2426c5788"),
-            hex!("fabb0ac9d68b0b445fb7357272ff202c5651694a"),
-            hex!("1cbd3b2770909d4e10f157cabc84c7264073c9ec"),
-            hex!("df3e18d64bc6a983f673ab319ccae4f1a57c7097"),
-            hex!("cd3b766ccdd6ae721141f452c550ca635964ce71"),
-            hex!("2546bcd3c84621e976d8185a91a922ae77ecec30"),
-            hex!("bda5747bfd65f08deb54cb465eb87d40e51b197e"),
-            hex!("dd2fd4581271e230360230f9337d5c0430bf44c0"),
-            hex!("8626f6940e2eb28930efb4cef49b2d1f2c9c1199"),
-        ]
-        .into_iter()
-        .map(|a| Address::from_slice(&a)),
-    )
+    [
+        hex!("f39fd6e51aad88f6f4ce6ab8827279cfffb92266"),
+        hex!("70997970c51812dc3a010c7d01b50e0d17dc79c8"),
+        hex!("3c44cdddb6a900fa2b585dd299e03d12fa4293bc"),
+        hex!("90f79bf6eb2c4f870365e785982e1f101e93b906"),
+        hex!("15d34aaf54267db7d7c367839aaf71a00a2c6a65"),
+        hex!("9965507d1a55bcc2695c58ba16fb37d819b0a4dc"),
+        hex!("976ea74026e726554db657fa54763abd0c3a0aa9"),
+        hex!("14dc79964da2c08b23698b3d3cc7ca32193d9955"),
+        hex!("23618e81e3f5cdf7f54c3d65f7fbc0abf5b21e8f"),
+        hex!("a0ee7a142d267c1f36714e4a8f75612f20a79720"),
+        hex!("bcd4042de499d14e55001ccbb24a551f3b954096"),
+        hex!("71be63f3384f5fb98995898a86b02fb2426c5788"),
+        hex!("fabb0ac9d68b0b445fb7357272ff202c5651694a"),
+        hex!("1cbd3b2770909d4e10f157cabc84c7264073c9ec"),
+        hex!("df3e18d64bc6a983f673ab319ccae4f1a57c7097"),
+        hex!("cd3b766ccdd6ae721141f452c550ca635964ce71"),
+        hex!("2546bcd3c84621e976d8185a91a922ae77ecec30"),
+        hex!("bda5747bfd65f08deb54cb465eb87d40e51b197e"),
+        hex!("dd2fd4581271e230360230f9337d5c0430bf44c0"),
+        hex!("8626f6940e2eb28930efb4cef49b2d1f2c9c1199"),
+    ]
+    .into_iter()
+    .map(|a| Address::from_slice(&a))
+    .collect()
 });
 
 /// The result of executing an `eth_call`.
