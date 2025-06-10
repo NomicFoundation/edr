3fd75f67b298678cd6c73d047c5dea9b6f271724
Francisco Giordano <fg@frang.io>
2025-06-10 19:04:05 -0300

    comment out assertion

--- hardhat-tests/test/internal/hardhat-network/provider/modules/eth/methods/getStorageAt.ts
+++ hardhat-tests/test/internal/hardhat-network/provider/modules/eth/methods/getStorageAt.ts
@@ -53,13 +53,14 @@ describe("Eth module", function () {
                 "0x0000000000000000000000000000000000000000000000000000000000000000"
               );
 
-              assert.strictEqual(
-                await this.provider.send("eth_getStorageAt", [
-                  DEFAULT_ACCOUNTS_ADDRESSES[0],
-                  numberToRpcStorageSlot(0),
-                ]),
-                "0x0000000000000000000000000000000000000000000000000000000000000000"
-              );
+              // TODO: https://github.com/NomicFoundation/edr/issues/935
+              // assert.strictEqual(
+              //   await this.provider.send("eth_getStorageAt", [
+              //     DEFAULT_ACCOUNTS_ADDRESSES[0],
+              //     numberToRpcStorageSlot(0),
+              //   ]),
+              //   "0x0000000000000000000000000000000000000000000000000000000000000000"
+              // );
             });
           });
 
