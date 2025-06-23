648f3bfd5027690f7a4571c4db2a217fb03e2504
Agost Biro <5764438+agostbiro@users.noreply.github.com>
2025-06-23 17:09:11 +0200

    test: add test for hh2 local edr version (#962)

--- crates/tools/js/benchmark/test/test-local-edr.ts
+++ crates/tools/js/benchmark/test/test-local-edr.ts
@@ -4,13 +4,12 @@ import { test } from "node:test";
 import path from "path";
 import { dirName } from "@nomicfoundation/edr-helpers";
 
-// The benchmarks assume that the EDR version used by
+// The provider scenario benchmarks assume that the EDR version used by
 // Hardhat is the one in the workspace, instead of the one installed
 // from npm. This test checks that this is the case.
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-test("uses the workspace version of EDR", function () {
+function checkHardhatEdrVersion(hardhatPackageName: string) {
   const require = createRequire(import.meta.url);
-  const hardhatPath = require.resolve("hardhat");
+  const hardhatPath = require.resolve(hardhatPackageName);
 
   const edrPath = require.resolve("@nomicfoundation/edr", {
     paths: [hardhatPath],
@@ -29,4 +28,16 @@ test("uses the workspace version of EDR", function () {
   );
 
   assert.equal(edrPath, expectedPath);
+}
+
+// False positive
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+test("Hardhat 2 uses the workspace version of EDR", function () {
+  checkHardhatEdrVersion("hardhat2");
+});
+
+// False positive
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+test("Hardhat 3 uses the workspace version of EDR", function () {
+  checkHardhatEdrVersion("hardhat");
 });
