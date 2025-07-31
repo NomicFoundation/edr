260e48a473df9c563d43101aef6c137eb4ec0b9e
Wodann <Wodann@users.noreply.github.com>
2025-07-31 18:23:19 +0000

    refactor: make SubscriptionEvent chain-agnostic

--- crates/edr_napi_core/src/subscription.rs
+++ crates/edr_napi_core/src/subscription.rs
@@ -1,23 +1,95 @@
-use derive_where::derive_where;
-use edr_eth::B256;
-use edr_provider::{time::CurrentTime, ProviderSpec, SubscriptionEvent};
+use std::sync::Arc;
+
+use edr_eth::{filter::LogOutput, B256, U256};
+use edr_evm::BlockAndTotalDifficulty;
 use napi::{
     threadsafe_function::{
         ErrorStrategy, ThreadSafeCallContext, ThreadsafeFunction, ThreadsafeFunctionCallMode,
     },
-    JsFunction,
+    JsFunction, JsUnknown,
 };
 
-#[derive_where(Clone)]
-pub struct Callback<ChainSpecT: ProviderSpec<CurrentTime> + 'static> {
-    inner: ThreadsafeFunction<SubscriptionEvent<ChainSpecT>, ErrorStrategy::Fatal>,
+/// A chain-agnostic version of [`edr_provider::SubscriptionEvent`].
+pub struct SubscriptionEvent {
+    pub filter_id: U256,
+    pub result: SubscriptionEventData,
 }
 
-impl<ChainSpecT: ProviderSpec<CurrentTime>> Callback<ChainSpecT> {
+impl SubscriptionEvent {
+    pub fn new<BlockT, RpcBlockT, SignedTransactionT>(
+        event: edr_provider::SubscriptionEvent<BlockT, SignedTransactionT>,
+    ) -> Self
+    where
+        BlockT: ?Sized + 'static,
+        RpcBlockT:
+            From<BlockAndTotalDifficulty<Arc<BlockT>, SignedTransactionT>> + serde::Serialize,
+        SignedTransactionT: 'static,
+    {
+        let edr_provider::SubscriptionEvent { filter_id, result } = event;
+
+        Self {
+            filter_id,
+            result: SubscriptionEventData::new::<_, RpcBlockT, _>(result),
+        }
+    }
+}
+
+/// Type alias for a function that converts a value to a JavaScript value.
+///
+/// This is used because [`serde::Serialize`] cannot be used as a dynamic trait
+/// object.
+///
+/// An alternative would be to use `serde_json::Value` as an intermediate
+/// representation, but that would require an additional conversion step.
+pub type ToJsValueFn = dyn FnOnce(&napi::Env) -> napi::Result<JsUnknown>;
+
+/// A chain-agnostic version of [`edr_provider::SubscriptionEventData`].
+pub enum SubscriptionEventData {
+    Logs(Vec<LogOutput>),
+    /// A function that converts a [`BlockAndTotalDifficulty`] to a JS value.
+    NewHeads(Box<ToJsValueFn>),
+    NewPendingTransactions(B256),
+}
+
+impl SubscriptionEventData {
+    pub fn new<BlockT, RpcBlockT, SignedTransactionT>(
+        data: edr_provider::SubscriptionEventData<BlockT, SignedTransactionT>,
+    ) -> Self
+    where
+        BlockT: ?Sized + 'static,
+        RpcBlockT:
+            From<BlockAndTotalDifficulty<Arc<BlockT>, SignedTransactionT>> + serde::Serialize,
+        SignedTransactionT: 'static,
+    {
+        match data {
+            edr_provider::SubscriptionEventData::Logs(log_outputs) => Self::Logs(log_outputs),
+            edr_provider::SubscriptionEventData::NewHeads(block_and_total_difficulty) => {
+                let block_to_js_value_fn = Box::new(move |env: &napi::Env| {
+                    let block = RpcBlockT::from(block_and_total_difficulty);
+
+                    env.to_js_value(&block)
+                        .map_err(|error| napi::Error::from_reason(error.to_string()))
+                });
+
+                Self::NewHeads(block_to_js_value_fn)
+            }
+            edr_provider::SubscriptionEventData::NewPendingTransactions(fixed_bytes) => {
+                Self::NewPendingTransactions(fixed_bytes)
+            }
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct Callback {
+    inner: ThreadsafeFunction<SubscriptionEvent, ErrorStrategy::Fatal>,
+}
+
+impl Callback {
     pub fn new(env: &napi::Env, subscription_event_callback: JsFunction) -> napi::Result<Self> {
         let mut callback = subscription_event_callback.create_threadsafe_function(
             0,
-            |ctx: ThreadSafeCallContext<SubscriptionEvent<ChainSpecT>>| {
+            |ctx: ThreadSafeCallContext<SubscriptionEvent>| {
                 // SubscriptionEvent
                 let mut event = ctx.env.create_object()?;
 
@@ -26,12 +98,11 @@ impl<ChainSpecT: ProviderSpec<CurrentTime>> Callback<ChainSpecT> {
                     .and_then(|filter_id| event.set_named_property("filterId", filter_id))?;
 
                 let result = match ctx.value.result {
-                    edr_provider::SubscriptionEventData::Logs(logs) => ctx.env.to_js_value(&logs),
-                    edr_provider::SubscriptionEventData::NewHeads(block) => {
-                        let block = ChainSpecT::RpcBlock::<B256>::from(block);
-                        ctx.env.to_js_value(&block)
+                    SubscriptionEventData::Logs(logs) => ctx.env.to_js_value(&logs),
+                    SubscriptionEventData::NewHeads(block_to_js_value_fn) => {
+                        block_to_js_value_fn(&ctx.env)
                     }
-                    edr_provider::SubscriptionEventData::NewPendingTransactions(tx_hash) => {
+                    SubscriptionEventData::NewPendingTransactions(tx_hash) => {
                         ctx.env.to_js_value(&tx_hash)
                     }
                 }?;
@@ -49,7 +120,7 @@ impl<ChainSpecT: ProviderSpec<CurrentTime>> Callback<ChainSpecT> {
         Ok(Self { inner: callback })
     }
 
-    pub fn call(&self, event: SubscriptionEvent<ChainSpecT>) {
+    pub fn call(&self, event: SubscriptionEvent) {
         // This is blocking because it's important that the subscription events are
         // in-order
         self.inner.call(event, ThreadsafeFunctionCallMode::Blocking);
