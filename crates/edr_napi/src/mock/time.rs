2f124187026dec70112d97ca50ff2d6e8deac49c
Wodann <Wodann@users.noreply.github.com>
2025-08-01 00:32:35 +0000

    fix: prevent deadlock by moving work to background thread

--- crates/edr_napi/src/mock/time.rs
+++ crates/edr_napi/src/mock/time.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use edr_generic::GenericChainSpec;
 use edr_napi_core::logger::Logger;
 use edr_solidity::contract_decoder::ContractDecoder;
-use napi::{bindgen_prelude::BigInt, tokio::runtime, Env};
+use napi::{bindgen_prelude::BigInt, tokio::runtime, Env, JsObject};
 use napi_derive::napi;
 
 use crate::{
@@ -49,53 +49,83 @@ pub fn create_provider_with_mock_timer(
     subscription_config: SubscriptionConfig,
     tracing_config: TracingConfigWithBuffers,
     time: &MockTime,
-) -> napi::Result<Provider> {
+) -> napi::Result<JsObject> {
+    let (deferred, promise) = env.create_deferred()?;
+
+    macro_rules! try_or_reject_promise {
+        ($expr:expr) => {
+            match $expr {
+                Ok(value) => value,
+                Err(error) => {
+                    deferred.reject(error);
+                    return Ok(promise);
+                }
+            }
+        };
+    }
+
     let runtime = runtime::Handle::current();
+    let provider_config = try_or_reject_promise!(provider_config.resolve(&env, runtime.clone()));
 
-    let provider_config = provider_config.resolve(&env, runtime.clone())?;
-    let provider_config =
-        edr_provider::ProviderConfig::<edr_eth::l1::SpecId>::try_from(provider_config)?;
-
-    let logger_config = logger_config.resolve(&env)?;
+    let logger_config = try_or_reject_promise!(logger_config.resolve(&env));
 
     // TODO: https://github.com/NomicFoundation/edr/issues/760
-    let build_info_config = edr_solidity::artifacts::BuildInfoConfig::parse_from_buffers(
-        (&edr_napi_core::solidity::config::TracingConfigWithBuffers::from(tracing_config)).into(),
-    )
-    .map_err(|error| napi::Error::from_reason(error.to_string()))?;
+    let build_info_config = try_or_reject_promise!(
+        edr_solidity::artifacts::BuildInfoConfig::parse_from_buffers(
+            (&edr_napi_core::solidity::config::TracingConfigWithBuffers::from(tracing_config))
+                .into(),
+        )
+        .map_err(|error| napi::Error::from_reason(error.to_string()))
+    );
 
-    let contract_decoder = ContractDecoder::new(&build_info_config)
-        .map_err(|error| napi::Error::from_reason(error.to_string()))?;
-
-    let contract_decoder = Arc::new(contract_decoder);
-
-    let logger = Logger::<GenericChainSpec, Arc<edr_provider::time::MockTime>>::new(
-        logger_config,
-        Arc::clone(&contract_decoder),
-    )?;
+    let contract_decoder = try_or_reject_promise!(ContractDecoder::new(&build_info_config)
+        .map_or_else(
+            |error| Err(napi::Error::from_reason(error.to_string())),
+            |contract_decoder| Ok(Arc::new(contract_decoder))
+        ));
 
     let subscription_config = edr_napi_core::subscription::Config::from(subscription_config);
-    let subscription_callback = edr_napi_core::subscription::Callback::new(
+    let subscription_callback = try_or_reject_promise!(edr_napi_core::subscription::Callback::new(
         &env,
         subscription_config.subscription_callback,
-    )?;
+    ));
 
-    let provider =
-        edr_provider::Provider::<GenericChainSpec, Arc<edr_provider::time::MockTime>>::new(
-            runtime.clone(),
-            Box::new(logger),
-            Box::new(move |event| subscription_callback.call(event)),
-            provider_config,
-            contract_decoder.clone(),
-            Arc::clone(&time.inner),
-        )
-        .map_err(|error| napi::Error::from_reason(error.to_string()))?;
+    let logger = try_or_reject_promise!(Logger::<
+        GenericChainSpec,
+        Arc<edr_provider::time::MockTime>,
+    >::new(logger_config, Arc::clone(&contract_decoder),));
 
-    Ok(Provider::new(
-        Arc::new(provider),
-        runtime,
-        contract_decoder,
-        #[cfg(feature = "scenarios")]
-        None,
-    ))
+    let provider_config = try_or_reject_promise!(
+        edr_provider::ProviderConfig::<edr_eth::l1::SpecId>::try_from(provider_config)
+    );
+
+    let timer = Arc::clone(&time.inner);
+
+    runtime.clone().spawn_blocking(move || {
+        let result =
+            edr_provider::Provider::<GenericChainSpec, Arc<edr_provider::time::MockTime>>::new(
+                runtime.clone(),
+                Box::new(logger),
+                Box::new(move |event| subscription_callback.call(event)),
+                provider_config,
+                contract_decoder.clone(),
+                timer,
+            )
+            .map_or_else(
+                |error| Err(napi::Error::from_reason(error.to_string())),
+                |provider| {
+                    Ok(Provider::new(
+                        Arc::new(provider),
+                        runtime,
+                        contract_decoder,
+                        #[cfg(feature = "scenarios")]
+                        None,
+                    ))
+                },
+            );
+
+        deferred.resolve(|_env| result);
+    });
+
+    Ok(promise)
 }
