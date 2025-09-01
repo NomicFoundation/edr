ca02ec81ca656eccdf2b5c5a08d84eab8a010f28
Ana Perez Ghiglia <aghiglia@manas.tech>
2025-09-01 13:18:23 -0300

    Add new integration tests for dynamic eip1559 fields in OP

--- crates/edr_op/tests/integration/full_block.rs
+++ crates/edr_op/tests/integration/full_block.rs
@@ -1,50 +1,24 @@
 #![cfg(feature = "test-remote")]
 
-use std::sync::LazyLock;
-
-use edr_eth::{
-    block::{self, HeaderOverrides},
-    eips::eip1559::{
-        BaseFeeActivation, BaseFeeParams, ConstantBaseFeeParams, VariableBaseFeeParams,
-    },
-};
+use edr_eth::block::{self, HeaderOverrides};
 use edr_evm::impl_full_block_tests;
 use edr_op::{test_utils::isthmus_header_overrides, OpChainSpec, OpSpecId};
 use edr_provider::test_utils::header_overrides;
 
 use super::op::mainnet_url;
 
-static OP_BASE_FEE_PARAMS: LazyLock<BaseFeeParams<OpSpecId>> = LazyLock::new(|| {
-    BaseFeeParams::Variable(VariableBaseFeeParams::new(vec![
-        (
-            BaseFeeActivation::Hardfork(OpSpecId::BEDROCK),
-            ConstantBaseFeeParams::new(50, 6),
-        ),
-        (
-            BaseFeeActivation::Hardfork(OpSpecId::CANYON),
-            ConstantBaseFeeParams::new(250, 6),
-        ),
-        // TODO: document that on OP, the base_fee params get activated not on the first block
-        // that has the fields in the extra_data, but on the next block, since if Holocene is
-        // activated then it uses the fields decoded from `parent_header.extraData`.
-        // EDR will consider the activation point as an inclusive range
-        // On OP mainnet, the first block to have extra_data field with (250, 4) is 135_513_415
-        // but we are configuring 135_513_416 here since ..416 is the first block to which those
-        // params must be applied
-        (
-            BaseFeeActivation::BlockNumber(135_513_416),
-            ConstantBaseFeeParams::new(250, 4),
-        ), // ConfigUpdate timestamp: 0x681b63b7, logIndex: 0x1e8
-        (
-            BaseFeeActivation::BlockNumber(136_165_876),
-            ConstantBaseFeeParams::new(250, 2),
-        ), // ConfigUpdate timestamp: 0x682f4d53, logIndex: 0x1ed
-    ]))
-});
-
 fn op_header_overrides(replay_header: &block::Header) -> HeaderOverrides<OpSpecId> {
     HeaderOverrides {
-        base_fee_params: Some(OP_BASE_FEE_PARAMS.clone()),
+        // We need to override extra_data since it can contain a base_fee_param update
+        // On OP stack chains, when a a SystemConfig eip-1559 fields update happens,
+        // the change gets evidenced first in a block extra_data field
+        // which will then used for calculating next block base_fee
+        //
+        // > Placing the EIP-1559 parameters within the L2 block header allows us to retain
+        // the purity of the function that computes the next block's base fee from its parent block
+        // header, while still allowing them to be dynamically configured
+        // see <https://specs.optimism.io/protocol/holocene/exec-engine.html>
+        extra_data: Some(replay_header.extra_data.clone()),
         ..header_overrides(replay_header)
     }
 }
