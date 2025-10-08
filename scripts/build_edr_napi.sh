43fd0c7a8ea556aa0c787747d269059b474af523
Wodann <Wodann@users.noreply.github.com>
2025-10-08 20:41:49 +0000

    build: use --locked for all cargo commands in CI and locally

--- scripts/build_edr_napi.sh
+++ scripts/build_edr_napi.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 # NAPI build must be done before the TypeScript compilation
-napi build --platform --no-const-enum "$@"
+napi build --platform --no-const-enum --cargo-flags="--locked" "$@"
 tsc
