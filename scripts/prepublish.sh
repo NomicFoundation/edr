cbd0f59494c60079559c8647bf96b96509325328
Bas van Gijzel <bas@nomic.foundation>
2025-10-21 17:39:36 +0100

    Add back optionalDeps -> deps

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -7,4 +7,4 @@ pnpm napi prepublish -t npm --skip-gh-release
 
 # temporarily commented to be able to publish in the @ignored org locally with verdaccio
 # rename optionalDependencies key to dependencies
-# jq 'with_entries(if .key == "optionalDependencies" then .key = "dependencies" else . end)' package.json | sponge package.json
+jq 'with_entries(if .key == "optionalDependencies" then .key = "dependencies" else . end)' package.json | sponge package.json
