a71ff7280576840840545d1b8e897db5a6de3dfc
Ana Perez Ghiglia <aghiglia@manas.tech>
2025-10-21 20:36:55 +0000

    Polish scripts comments

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -5,6 +5,4 @@ set -o pipefail
 
 pnpm napi prepublish -t npm --skip-gh-release
 
-# temporarily commented to be able to publish in the @ignored org locally with verdaccio
-# rename optionalDependencies key to dependencies
 jq 'with_entries(if .key == "optionalDependencies" then .key = "dependencies" else . end)' package.json | sponge package.json
