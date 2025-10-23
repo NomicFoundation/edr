98f1ad3be79cb0578e71b9eaeca2eac918b273f4
Ana Perez Ghiglia <aghiglia@manas.tech>
2025-10-23 17:58:58 +0000

    Use `napi version` command instead of prepublish

--- scripts/pre_release.sh
+++ scripts/pre_release.sh
@@ -13,22 +13,8 @@ fi
 # so platform-specific packages are updated with the current version
 pnpm changeset version
 
-# Add changes to staged area
-git add .
-
 cd ./crates/edr_napi
-# Run napi prepublish to update platform-specific `package.json` files
-pnpm napi prepublish -t npm --skip-gh-release
-
-# Ignore changes done to napi root `package.json`
-# The command adds the `optionalDependencies` field
-# We don't want to include it because adding it would make the 
-# pnpm-lock.yaml differ with package.json - and `pnpm install` does
-# not fix the lockfile in this case.
-# TODO: NAPI.rs v3 has a skipOptionalPublish option
-git restore ./package.json 
+# Run `napi version` to update version in cross-platform packages
+pnpm napi version
 
 cd ../.. 
-
-# Leave all changes unstaged for commit by the Changesets CI action
-git restore --staged .
