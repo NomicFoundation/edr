861d7dbee4a2923fe49b0da648e43becf6df9e05
Octavian Popescu <popescu.octavian09@gmail.com>
2025-11-03 18:55:50 +0000

    windows 4

--- scripts/build_edr_napi.sh
+++ scripts/build_edr_napi.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 # NAPI build must be done before the TypeScript compilation
-napi build --platform --no-const-enum "$@"
+node "$(dirname "$0")/../node_modules/@napi-rs/cli/scripts/index.js"  build --platform --no-const-enum "$@"
 tsc
