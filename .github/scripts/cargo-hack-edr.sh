b9c49549c531a37140a0dcecd72f092ae8b4db38
Wodann <Wodann@users.noreply.github.com>
2025-11-12 15:19:44 -0600

    build: use --locked for all cargo commands in CI and locally (#1121)

--- .github/scripts/cargo-hack-edr.sh
+++ .github/scripts/cargo-hack-edr.sh
@@ -6,7 +6,7 @@ set -e
 for dir in crates/edr_*/ ; do
     if [ -d "$dir" ]; then
       pushd "$dir" > /dev/null 
-      cargo hack check --feature-powerset --exclude-no-default-features --no-dev-deps 
+      cargo hack check --feature-powerset --exclude-no-default-features --no-dev-deps
       popd > /dev/null
     fi
 done
