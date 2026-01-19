a8e67d992738c9fc85d4c79eb605be3bf3848138
Octavian Popescu <popescu.octavian09@gmail.com>
2026-01-19 10:29:48 +0000

    napi errors

---
+++ crates/edr_napi/src/solidity_tests/cheatcode_errors.rs
@@ -0,0 +1,19 @@
+use napi_derive::napi;
+
+#[napi(string_enum)]
+#[doc = "Error codes that can be returned by cheatcodes in Solidity tests."]
+pub enum CheatcodeErrorCode {
+    #[doc = "The specified cheatcode is not supported."]
+    UnsupportedCheatcode,
+    #[doc = "The specified cheatcode is missing"]
+    MissingCheatcode,
+    // TODO: Add more error codes as needed.
+}
+
+#[napi(object)]
+#[doc = "Represents an error returned by a cheatcode in Solidity tests."]
+pub struct CheatcodeErrorDetails {
+    #[doc = "The error code representing the type of cheatcode error."]
+    pub code: CheatcodeErrorCode,
+    #[doc = "The name of the cheatcode that caused the error."]
+}
