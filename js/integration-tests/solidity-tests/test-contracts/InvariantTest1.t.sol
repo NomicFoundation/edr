a686a18afe0fd9dd4bb45a083bd072971c4e945e
Octavian Popescu <popescu.octavian09@gmail.com>
2026-01-21 21:28:15 +0000

    fixed flaky test

--- js/integration-tests/solidity-tests/test-contracts/InvariantTest1.t.sol
+++ js/integration-tests/solidity-tests/test-contracts/InvariantTest1.t.sol
@@ -4,18 +4,10 @@ pragma solidity ^0.8.18;
 import {Test} from "forge-std/src/Test.sol";
 
 contract InvariantBreaker {
-    bool public flag0 = true;
     bool public flag1 = true;
 
-    function set0(int256 val) public returns (bool) {
-        if (val % 100 == 0) {
-            flag0 = false;
-        }
-        return flag0;
-    }
-
     function set1(int256 val) public returns (bool) {
-        if (val % 10 == 0 && !flag0) {
+        if (val % 10 == 0 && val % 10 == 1) {
             flag1 = false;
         }
         return flag1;
