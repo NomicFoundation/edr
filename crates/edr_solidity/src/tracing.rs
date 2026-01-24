12cf692531b1016b58b7df6059936589a5d2601e
Wodann <Wodann@users.noreply.github.com>
2026-01-24 17:35:02 +0000

    WIP: fix: missing files

---
+++ crates/edr_solidity/src/tracing.rs
@@ -0,0 +1,44 @@
+//! Types and utilities for tracing EVM execution with Solidity-specific
+//! decoding.
+
+use std::sync::Arc;
+
+use edr_primitives::{Address, Bytes, HashMap, HashSet};
+use revm_inspectors::tracing::{CallTraceArena, TracingInspector};
+
+use crate::contract_decoder::ContractDecoder;
+
+/// A tracing inspector that uses a [`ContractDecoder`] to decode
+/// Solidity-specific information.
+///
+/// The [`TracingInspector`] does not store the bytecode of executed bytecode
+/// for call transactions, so we need to store them here to be able to decode
+/// the traces properly.
+pub struct SolidityTracingInspector {
+    address_to_runtime_code: HashMap<Address, Bytes>,
+    decoder: Arc<ContractDecoder>,
+    inspector: TracingInspector,
+}
+
+impl SolidityTracingInspector {
+    /// Constructs a new [`SolidityTracingInspector`] instance.
+    pub fn new(inspector: TracingInspector, decoder: Arc<ContractDecoder>) -> Self {
+        Self {
+            decoder,
+            address_to_runtime_code: HashMap::default(),
+            inspector,
+        }
+    }
+
+    pub fn collect(self, precompiles: HashSet<Address>) -> CallTraceArena {
+        let mut arena = self.inspector.into_traces();
+
+        for node in arena.nodes_mut() {
+            // self.decoder
+            //     .populate_call_trace(&mut node.trace, code,
+            // precompile_spec_id);
+        }
+
+        arena
+    }
+}
