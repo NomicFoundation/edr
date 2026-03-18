04949c5e5c64f52bb5576f5b9dc107a3e047717a
Wodann <Wodann@users.noreply.github.com>
2026-03-18 16:49:49 +0000

    research: figure out how to fix returndata opcodes bug

--- crates/edr_coverage/src/collector.rs
+++ crates/edr_coverage/src/collector.rs
@@ -1,5 +1,5 @@
 use edr_chain_spec_evm::{
-    interpreter::{CallInputs, CallOutcome, InterpreterTypes},
+    interpreter::{CallInputs, CallOutcome, InputsTr as _, InterpreterTypes, ReturnData as _},
     ContextTrait, Inspector,
 };
 use edr_primitives::{Bytes, HashSet};
@@ -9,6 +9,9 @@ use crate::COVERAGE_ADDRESS;
 #[derive(Clone, Debug, Default)]
 pub struct CoverageHitCollector {
     hits: HashSet<Bytes>,
+    /// Stores the return data of the previous call, which is used by
+    /// `returndatacopy` and `returndatasize`.
+    previous_call_return_data: Bytes,
 }
 
 impl CoverageHitCollector {
@@ -31,11 +34,35 @@ impl CoverageHitCollector {
 impl<ContextT: ContextTrait, InterpreterT: InterpreterTypes> Inspector<ContextT, InterpreterT>
     for CoverageHitCollector
 {
+    fn initialize_interp(
+        &mut self,
+        interp: &mut edr_chain_spec_evm::interpreter::Interpreter<InterpreterT>,
+        _context: &mut ContextT,
+    ) {
+        self.previous_call_return_data = interp.return_data.buffer().clone();
+        println!(
+            "initialize_interp ({:?}): {:?}",
+            interp.input.bytecode_address(),
+            self.previous_call_return_data
+        );
+    }
+
     fn call(&mut self, context: &mut ContextT, inputs: &mut CallInputs) -> Option<CallOutcome> {
+        println!("call: {}", inputs.bytecode_address);
         if inputs.bytecode_address == COVERAGE_ADDRESS {
             self.record_hit(inputs.input.bytes(context));
         }
 
         None
+    }
+
+    fn call_end(
+        &mut self,
+        _context: &mut ContextT,
+        _inputs: &CallInputs,
+        outcome: &mut CallOutcome,
+    ) {
+        println!("call end: {}", _inputs.bytecode_address);
+        outcome.result.output = self.previous_call_return_data.clone();
     }
 }
