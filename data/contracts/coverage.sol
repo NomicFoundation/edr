3d360dd356c5771d0d78bde2f02a8b8fc612c904
Ana Perez Ghiglia <aghiglia@manas.tech>
2026-03-30 22:22:42 +0000

    Revert "Narrow down coverage.sol pragma declaration"
    This reverts commit f889e56b81c2d6b09913407dc5d13894915cef80.

--- data/contracts/coverage.sol
+++ data/contracts/coverage.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26; // This is different from Hardhat definition
+pragma solidity >=0.4.22 <0.9.0;
 
 library __HardhatCoverage {
   address constant COVERAGE_ADDRESS =
